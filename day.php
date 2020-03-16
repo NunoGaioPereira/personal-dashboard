590dd20e3061afe52578dabd6d32a83842e896d3
Nuno Pereira <nunocgpereira@gmail.com>
2020-03-16 22:41:56 +0000

    feat(day): add day selector

--- day.php
+++ day.php
@@ -2,6 +2,54 @@
 	require_once('includes/nav.inc.php');
 ?>
 
+	<div class="main-wrapper">
+		<h2>Mar 2020</h2>
+
+		<div class="day-slider">
+			<a href="">
+				<div class="day-selector">
+					<p class="day">22</p>
+					<p class="week-day">M</p>
+				</div>
+			</a>
+			<a href="">
+				<div class="day-selector">
+					<p class="day">23</p>
+					<p class="week-day">T</p>
+				</div>
+			</a>
+			<a href="">
+				<div class="day-selector">
+					<p class="day">24</p>
+					<p class="week-day">W</p>
+				</div>
+			</a>
+			<a href="">
+				<div class="day-selector active">
+					<p class="day">25</p>
+					<p class="week-day">T</p>
+				</div>
+			</a>
+			<a href="">
+				<div class="day-selector">
+					<p class="day">26</p>
+					<p class="week-day">F</p>
+				</div>
+			</a>
+			<a href="">
+				<div class="day-selector">
+					<p class="day">27</p>
+					<p class="week-day">S</p>
+				</div>
+			</a>
+			<a href="">
+				<div class="day-selector">
+					<p class="day">28</p>
+					<p class="week-day">S</p>
+				</div>
+			</a>
+		</div>
+	</div>
 
 	<script src="js/main.js"></script>
 </body>
