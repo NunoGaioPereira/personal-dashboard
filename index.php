9080699c0e250e860b0a9942f8350339755771ce
Nuno Pereira <nunocgpereira@gmail.com>
2020-03-16 22:25:18 +0000

    refact: changed navigation to separate file and created day page

--- index.php
+++ index.php
@@ -1,50 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-	<link rel="stylesheet" type="text/css" href="css/main.css">
-</head>
-<body>
-
-	<div id="popup">
-		<h2>Welcome</h2>
-		<p>We are working hard to let you enjoy this process. This card will let you decide what next action you wish to take.</p>
-		<p><a href="#/">Read More</a></p>
-	</div>
-
-	<div id="extra-nav">
-		<a href="#/" class="nav-close"><img src="./includes/imgs/cross.png"></a>
-		<h2>Your Apps</h2>
-		<ul>
-			<li><a href="">Home</a></li>
-			<li><a href="">Home</a></li>
-			<li><a href="">Home</a></li>
-		</ul>
-	</div>
-
-	<div id="side-nav">
-		<div class="hamburguer"><a href="#/"><img src="./includes/imgs/hamburguer.png"></a></div>
-		<ul>
-			<a href="#/" class="active blah"><img src="./includes/imgs/home.png"></a>
-			<a href=""><img src="./includes/imgs/stats.png"></a>
-			<a href=""><img src="./includes/imgs/edit.png"></a>
-			<a href=""><img src="./includes/imgs/posts.png"></a>
-		</ul>
-		<div>
-			<form>
-				<a type="submit" name="logout-submit" class="logout" title="Logout"> <img src="includes/imgs/logout.png"></a>
-			</form>
-		</div>
-	</div>
-
-	<div id="moving-nav">
-		<div class="content">
-			<h2>Menu</h2>
-		</div>
-	</div>
-
-	
-
+<?php
+	require_once('includes/nav.inc.php');
+?>
 	<div class="main-wrapper">
 
 		<div id="cover"></div>
