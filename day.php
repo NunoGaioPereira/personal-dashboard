b94fdc82d7d633cad95638128c0c1d1eaa0da601
Nuno Pereira <nunocgpereira@gmail.com>
2020-03-18 19:57:31 +0000

    style: wrapped day table and slider

--- day.php
+++ day.php
@@ -5,52 +5,78 @@
 	<div class="main-wrapper">
 		<h2>Mar 2020</h2>
 
-		<div class="day-slider">
-			<a href="">
-				<div class="day-selector">
-					<p class="day">22</p>
-					<p class="week-day">M</p>
+		<div class="holder">
+			<div class="day-slider">
+				<a href="">
+					<div class="day-selector">
+						<p class="day">22</p>
+						<p class="week-day">M</p>
+					</div>
+				</a>
+				<a href="">
+					<div class="day-selector">
+						<p class="day">23</p>
+						<p class="week-day">T</p>
+					</div>
+				</a>
+				<a href="">
+					<div class="day-selector">
+						<p class="day">24</p>
+						<p class="week-day">W</p>
+					</div>
+				</a>
+				<a href="">
+					<div class="day-selector active">
+						<p class="day">25</p>
+						<p class="week-day">T</p>
+					</div>
+				</a>
+				<a href="">
+					<div class="day-selector">
+						<p class="day">26</p>
+						<p class="week-day">F</p>
+					</div>
+				</a>
+				<a href="">
+					<div class="day-selector">
+						<p class="day">27</p>
+						<p class="week-day">S</p>
+					</div>
+				</a>
+				<a href="">
+					<div class="day-selector">
+						<p class="day">28</p>
+						<p class="week-day">S</p>
+					</div>
+				</a>
+			</div>
+			<div class="table-day">
+				<div class="header"><h4>Mar 25</h4></div>
+				<div class="entries">
+					<h4 class="title">Legacy</h4>
+					<input class="entry" type="text" name="le1" value="1. ">
+					<input class="entry" type="text" name="le2" value="2. ">
+					<input class="entry" type="text" name="le3" value="3. ">
+					<h4 class="title">Gratitude</h4>
+					<input class="entry" type="text" name="g1">
+					<input class="entry" type="text" name="g2">
+					<input class="entry" type="text" name="g3">
+					<h4 class="title">Self-love</h4>
+					<input class="entry" type="text" name="s1">
+					<input class="entry" type="text" name="s2">
+					<input class="entry" type="text" name="s3">
+					<h4 class="title">Blessings</h4>
+					<input class="entry" type="text" name="b1">
+					<input class="entry" type="text" name="b2">
+					<input class="entry" type="text" name="b3">
+					<h4 class="title">Learnings</h4>
+					<input class="entry" type="text" name="lr1">
+					<input class="entry" type="text" name="lr2">
 				</div>
-			</a>
-			<a href="">
-				<div class="day-selector">
-					<p class="day">23</p>
-					<p class="week-day">T</p>
-				</div>
-			</a>
-			<a href="">
-				<div class="day-selector">
-					<p class="day">24</p>
-					<p class="week-day">W</p>
-				</div>
-			</a>
-			<a href="">
-				<div class="day-selector active">
-					<p class="day">25</p>
-					<p class="week-day">T</p>
-				</div>
-			</a>
-			<a href="">
-				<div class="day-selector">
-					<p class="day">26</p>
-					<p class="week-day">F</p>
-				</div>
-			</a>
-			<a href="">
-				<div class="day-selector">
-					<p class="day">27</p>
-					<p class="week-day">S</p>
-				</div>
-			</a>
-			<a href="">
-				<div class="day-selector">
-					<p class="day">28</p>
-					<p class="week-day">S</p>
-				</div>
-			</a>
+			</div>
 		</div>
 
-		<div class="day-table-container">
+		<!-- <div class="day-table-container">
 			<table>	
 					<tr class="header"><th>25 May</th></tr>
 					<tr>
@@ -83,33 +109,8 @@
 					<tr><td contenteditable="true">1. </td></tr>
 					<tr><td contenteditable="true">2. </td></tr>
 			</table>
-			<!-- <h2 class="banana" contenteditable="true">Test</h2> -->
-		</div>
-
-		<div class="table-day">
-			<div class="header"><h4>25 February</h4></div>
-			<div class="entries">
-				<h4 class="title">Legacy</h4>
-				<input class="entry" type="text" name="le1">
-				<input class="entry" type="text" name="le2">
-				<input class="entry" type="text" name="le3">
-				<h4 class="title">Gratitude</h4>
-				<input class="entry" type="text" name="g1">
-				<input class="entry" type="text" name="g2">
-				<input class="entry" type="text" name="g3">
-				<h4 class="title">Self-love</h4>
-				<input class="entry" type="text" name="s1">
-				<input class="entry" type="text" name="s2">
-				<input class="entry" type="text" name="s3">
-				<h4 class="title">Blessings</h4>
-				<input class="entry" type="text" name="b1">
-				<input class="entry" type="text" name="b2">
-				<input class="entry" type="text" name="b3">
-				<h4 class="title">Learnings</h4>
-				<input class="entry" type="text" name="lr1">
-				<input class="entry" type="text" name="lr2">
-			</div>
-		</div>
+			<h2 class="banana" contenteditable="true">Test</h2>
+		</div> -->
 	</div>
 
 	<script src="js/main.js"></script>
